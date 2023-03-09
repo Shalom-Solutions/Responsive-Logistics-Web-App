c9bc84c8468495ace289af07b570c1ae10edcb6f
Shalom-Solutions <omogoyejuwon@gmail.com>
2023-03-09 07:42:01 +0100

    built the mobile version of a Logistics website

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu') 
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Remove Menu Mobile
+
+
+// Scroll Sections Active Link
+
+
+
+// Change background Header
+
+
+
+//Show Scroll Up
+
+
+
+//Dark Light Mode
